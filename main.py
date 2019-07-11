2334e95856c55a76cc4d12183fb55798fdf44c2d
Tariq Zaki <zakitariq24@gmail.com>
2019-07-11 17:22:16 +0100

    progressed through tutorial adding print statements to ensure all lines working as expected

--- main.py
+++ main.py
@@ -21,13 +21,46 @@ i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb 
 items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
 encoding='latin-1')
 
-print(users.head())
-print(ratings.head())
-print(items.head())
+#print(users.head())
+#print(ratings.head())
+#print(items.head())
 
 #import ratings training set
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
 ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
-print(ratings_train.shape)
-print(ratings_test.shape)
+#print(ratings_train.shape)
+#print(ratings_test.shape)
+
+#We will recommend movies based on user-user similarity and item-item similarity. For that, first we need to calculate the number of unique users and movies
+n_users = ratings.user_id.unique().shape[0]
+n_items = ratings.movie_id.unique().shape[0]
+print(n_users)
+print(n_items)
+#Now, we will create a user-item matrix which can be used to calculate the similarity between users and items.
+data_matrix = np.zeros((n_users, n_items))
+for line in ratings.itertuples():
+    data_matrix[line[1]-1, line[2]-1] = line[3]
+
+#Now, we will calculate the similarity. We can use the pairwise_distance function from sklearn to calculate the cosine similarity.
+from sklearn.metrics.pairwise import pairwise_distances
+user_similarity = pairwise_distances(data_matrix, metric='cosine')
+item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
+
+
+#This gives us the item-item and user-user similarity in an array form. The next step is to make predictions based on these similarities. Let’s define a function to do just that.
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        #We use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+user_prediction = predict(data_matrix, user_similarity, type='user')
+# item_prediction = predict(data_matrix, item_similarity, type='item')
+#print(ratings)
+print(data_matrix[307])
+print(user_prediction[307])
